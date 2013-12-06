16856ddb28f5744ea89a1c8a87b2db3262c8bb18
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2013-12-06 21:44:23 +0700

    add yahoo search

--- src/apps/yahoo/forms.py
+++ src/apps/yahoo/forms.py
@@ -17,14 +17,36 @@ class YahooSearchForm(forms.Form):
         ('Affiliate', 'Affiliate')
     )
 
+    ORDER_CHOICES = (
+        ('a', 'Ascending'),
+        ('d', 'Descending')
+    )
+
+    ITEM_STATUS_CHOICES = (
+        (0, 'Not specified'),
+        (1, 'New'),
+        (2, 'Used')
+    )
+
+    F_CHOICES = (
+        ('0x2', 'Search for "Title + store for Search"'),
+        ('0x4', 'Search for "Title + body"'),
+        ('0x8', 'Search for "title only"')
+    )
+
     # Get the search words entered in the form
     query = forms.CharField()
 
     type = forms.CharField(widget=forms.Select(choices=TYPE_CHOICES))
 
     sort = forms.CharField(widget=forms.Select(choices=SORT_CHOICES))
+    order = forms.CharField(widget=forms.Select(choices=ORDER_CHOICES))
+    category = forms.IntegerField()
+
     # Get the product state that is entered in the form
-    # item_status = forms.CharField()
+    item_status = forms.CharField(widget=forms.Select(choices=ITEM_STATUS_CHOICES))
+
+    f = forms.CharField(widget=forms.Select(choices=F_CHOICES))
 
     # Get the upper limit of the range specification of commodity prices entered in the form
     # aucmaxprice = forms.IntegerField()
--- src/apps/yahoo/views.py
+++ src/apps/yahoo/views.py
@@ -19,18 +19,24 @@ class YahooSearchView(FormView):
 
     def get_context_data(self, **kwargs):
         data = super(YahooSearchView, self).get_context_data(**kwargs)
-        print dir(kwargs['form'])
+        # print dir(kwargs['form'])
         if 'query' in self.initial and self.initial['query']:
             obj = Search('dj0zaiZpPXFONUl2dTR2ck5wYyZzPWNvbnN1bWVyc2VjcmV0Jng9YmY-', 'V2')
             obj.set_option('query', self.initial['query'])
             obj.set_option('type', self.initial['type'])
             obj.set_option('sort', self.initial['sort'])
+            obj.set_option('order', self.initial['order'])
+            obj.set_option('category', self.initial['category'])
+            obj.set_option('item_status', self.initial['item_status'])
+            obj.set_option('f', self.initial['f'])
             obj.set_option('output', 'json')
             obj.set_option('store', 0)
 
             response = obj.action()
 
             data['jsonp'] = response
-            data['items'] = response['ResultSet']['Result']['Item']
+            result = response['ResultSet']['Result']
+            if 'Item' in result:
+                data['items'] = result['Item']
 
         return data
